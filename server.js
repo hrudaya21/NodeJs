ffe1a13bcbcf06ebf39edc88b48c8a14bcd8ec9d
Hrudaya Ranjan Sahoo <hrudaya21@gmail.com>
2018-03-20 08:10:08 +0530

    Added Start Script and Heroku Port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
@@ -20,9 +21,10 @@ app.use( (req,res,next) => {
   next();
 });
 
-app.use( (req,res, next) => {
-  res.render('maintenance.hbs');
-});
+// app.use( (req,res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 //Write the Common Function to get the Current Year
 hbs.registerHelper('getCurrentYear', () => {
@@ -53,6 +55,6 @@ app.get('/bad', (req,res) => {
     errorMessage: 'Unable to Handle request'
   });
 })
-app.listen(3000, () => {
-  console.log('Server is up on Port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on Port ${port}`);
 });
