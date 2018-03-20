18f17430558748a422161cccaf8454d78b03b846
Hrudaya Ranjan Sahoo <hrudaya21@gmail.com>
2018-03-20 07:03:47 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view Engine', 'hbs');
+
+
+app.use( (req,res,next) => {
+  var now = new Date().toString();
+
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err){
+      console.log('Unable to Append to server.log');
+    }
+  })
+  next();
+});
+
+app.use( (req,res, next) => {
+  res.render('maintenance.hbs');
+});
+app.use(express.static(__dirname + '/public'));
+//Write the Common Function to get the Current Year
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+//A Global Function to Change the text to UpperCase
+hbs.registerHelper('scremlt', (text) => {
+  return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my Website',
+    //currentYear:new Date().getFullYear()
+  });
+
+});
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    //currentYear: new Date().getFullYear()
+  });
+})
+
+app.get('/bad', (req,res) => {
+  res.send({
+    errorMessage: 'Unable to Handle request'
+  });
+})
+app.listen(3000, () => {
+  console.log('Server is up on Port 3000');
+});
